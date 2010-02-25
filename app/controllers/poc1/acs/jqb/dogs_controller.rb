992d27cdc2dc221097e9b530f2b6b186b79f1519
Joe Goggins <joe.goggins@gmail.com>
2010-02-24 21:43:10 -0600

    Added jquery_firebug so I can log directly to the firebug console--super handy

--- app/controllers/poc1/acs/jqb/dogs_controller.rb
+++ app/controllers/poc1/acs/jqb/dogs_controller.rb
@@ -2,8 +2,17 @@ class Poc1::Acs::Jqb::DogsController < ApplicationController
   # Yields json results given ?q=<SEARCH_TERM>
   #
   def index
-    respond_to do |format|
-      format.json {render :json => {
+    @stub1 = [{
+        :model => "Dog #{rand}",
+        :primary_keys => %w(id),
+        :keys => %w(id),
+        :mapping => %w(id name),
+        :results => [
+          [1,"golden retreiver"],
+          [5,"golden donkey"]
+        ]
+      },
+      {
           :model => "Dog #{rand}",
           :primary_keys => %w(id),
           :keys => %w(id),
@@ -13,7 +22,9 @@ class Poc1::Acs::Jqb::DogsController < ApplicationController
             [5,"golden donkey"]
           ]
         }
-      } 
+      ]
+    respond_to do |format|
+      format.json {render :json => @stub1} 
     end
   end
   
@@ -33,10 +44,15 @@ class Poc1::Acs::Jqb::DogsController < ApplicationController
         <body>
         <input type="textfield" name="the_textfield" acs_source="/acs/jqb/dogs" id="the_textfield"/>
         
+        <input type="textfield" name="the_textfield2" acs_source="/acs/jqb/dogs" id="the_textfield2"/>
+        
         <%= javascript_include_tag 'jquery-1.4.2' %>
+         
+        
         <% javascript_tag do %>
-        // The most basic Autocompleter, now simply demonstrating it hits 
-        // the server on load
+        // Top Level function to bind an Auto-Completer to an HTML element
+        // Should eventually extract autocompleter client name from a acs_source like "/acs/jqb_v2/dogs"
+        // to invoke JqbV2.attach(element)
         jQuery.fn.extend({
           bindAcs:function() {
             this.each(function() {
@@ -44,38 +60,58 @@ class Poc1::Acs::Jqb::DogsController < ApplicationController
               Jqb.attach(element);
           })
         }
-        })
+        });
+        
+        // A namespace to hold all references to all Autocompleter client bindings
+        // on this page
+        var Acs = {
+          instances:[]
+        }
+        
+        // jQuery Basic Autocompleter for ACS API
+        //
         var Jqb = {
           loaded:false,
+          
+          // This is like the constructor--where all of the base-line behavior associated
+          // with a Jqb client can added to the page
           attach:function(element) {
+            // Move the 
             element.data("acs_source", element.attr('acs_source'));
             element.focus(function(){
-              //alert('fa fa focused' + element.data("acs_source"));
+              //Do nothing
             }),
-            element.keydown(function(event){
-              $.getJSON(element.data("acs_source"), function(json) {
-                alert(json.model);
-              })
-              /*
-              If the first character is ? and there are no characters, then
-              show the keyboard shortcut's for navigating the autocompleter
-              
-              Otherwise propogate the search term to the server
-              
-              */
-              switch (event.keyCode) {
-                // ...
-                // different keys do different things
-                // Different browsers provide different codes
-                // see here for details: http://unixpapa.com/js/key.html    
-                // ...
-              }
-              alert('po');
-              return true;
-              
+            element.keyup(this.handleKeyUp)
+          },
+          
+          handleKeyUp:function(event) {
+            var keyupped = event.target;
+            console.log('[handleKeyUp] ' + $(keyupped).val());
+            
+            // Catch arrows, enter, tab
+            // pass other stuff through to ajax request
+            // should buffer key strokes and wait for the user
+            // to finish typing (i think)
+            //
+            switch (event.keyCode) {
+              //Catch/buffer/ depending on the key pressed
+              // see here for details: http://unixpapa.com/js/key.html    
+            }
+            
+            // Fire off request
+            $.getJSON($(keyupped).attr('acs_source'), {q:$(keyupped).val()}, function(json) {
+              jQuery.each(json, function() {
+                console.log(this.model);
+              });
             })
           }
+          
+          
         }
+        
+        
+        // Starting point to attach the autocompleter server
+        // to all clients on the page
         $(document).ready(function(){
           $("input[acs_source]").bindAcs();
         });
--- app/models/poc1/dog.rb
+++ app/models/poc1/dog.rb
@@ -1,3 +1,12 @@
 class Poc1::Dog < ActiveRecord::Base
   set_table_name 'poc1_dogs'
+  
+  def self.acs_config
+    {:model => self.class.to_s,
+     :primary_keys => %w(id),
+     :keys => %w(id),
+     :mapping => %w(id name)
+    }
+  end  
+  
 end
